4efd0a9ae641769fec3a957a76118d4b57272ef0
Leela1201 <115172734+Leela1201@users.noreply.github.com>
2023-05-02 13:03:39 +0530

    2/05/2023

---
+++ BenchUpskilling/Remove_Firsrchars.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchUpskilling
+{
+    internal class Remove_Firsrchars
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter the two strings");
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+            string result = "";
+            foreach (char c in s1)
+            {
+                if (!s2.Contains(c))
+                {
+                    result += c;
+                }
+            }
+            Console.WriteLine("characters are :" + result);
+
+
+        }
+    }
+}
