d205aeedebea1548990143bc83fea8a26b31baa7
Leela1201 <115172734+Leela1201@users.noreply.github.com>
2023-05-02 13:06:24 +0530

    BenchUpskilling 2/05/2023

---
+++ BenchUpskilling/SortArray_Ascending.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchUpskilling
+{
+    internal class SortArray_Ascending
+    {
+        class Ascending_Number
+        {
+            public static void Main(string[] args)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+                string s = n.ToString();
+                char[] no = s.ToCharArray();
+                Array.Sort(no);
+
+                foreach (char c in no)
+                {
+                    Console.WriteLine(c);
+                }
+            }
+        }
+    }
+}
---
+++ BenchUpskilling/String_Ascending.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchUpskilling
+{
+    internal class String_Ascending
+    {
+        public static void Main(string[] args)
+        {
+            //to read the name and print the letters in ascending order
+            Console.WriteLine("enter the name: ");
+            string s = Console.ReadLine();
+            char[] name = s.ToCharArray();
+            Array.Sort(name);
+            string newS1 = new string(name);
+            Console.WriteLine("letters in ascending order");
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(newS1[i]);
+            }
+
+
+
+        }
+
+    }
+}
