e28abfa2b03a132cf8111f20cf15d973f3d45d33
Leela1201 <115172734+Leela1201@users.noreply.github.com>
2023-05-02 12:59:46 +0530

    2/05/2023

---
+++ BenchUpskilling/PowerOf_num.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchUpskilling
+{
+    internal class PowerOf_num
+    {
+        public static void Main(string[] args)
+        {
+            int baseNumber, expNumber, result = 1;
+            Console.Write("Base Number : ");
+            baseNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("exponent Number : ");
+            expNumber = Convert.ToInt32(Console.ReadLine());
+            while (expNumber != 0)
+            {
+                result *= baseNumber;
+                --expNumber;
+            }
+            Console.WriteLine("Result = {0}", result);
+            Console.ReadLine();
+        }
+    }
+}
